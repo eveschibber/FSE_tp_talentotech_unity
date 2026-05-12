181da741edd6b05b33028ea545403211ef10d15f
eveschibber <69548108+eveschibber@users.noreply.github.com>
2026-05-12 16:35:19 -0300

    cambie el suelo

--- Assets/scripts/SueloMovimiento.cs
+++ Assets/scripts/SueloMovimiento.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class SueloMovimiento : MonoBehaviour
 {
-    public float velocidad = 7f;
+    public float velocidad = 5f;
+    public float anchoPieza = 2.56f; // El ancho de UN solo bloquecito
+    public int cantidadBloques = 10; // Cuántos bloques tenés en fila
+    
+    public Sprite[] misDibujosDeSuelo; // Arrastrá acá todos tus sprites de fósiles, libros, etc.
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        // El suelo se mueve a la izquierda
+        // Mover a la izquierda
         transform.Translate(Vector2.left * velocidad * Time.deltaTime);
 
-        // Si el suelo se va de la pantalla, lo reposicionamos adelante (Loop)
-        if (transform.position.x < -20f)
+        // Si este bloque sale de pantalla
+        if (transform.position.x <= -anchoPieza)
         {
-            transform.position = new Vector3(20f, transform.position.y, transform.position.z);
+            // Lo movemos al final de la fila de bloques
+            float nuevaX = transform.position.x + (anchoPieza * cantidadBloques);
+            transform.position = new Vector3(nuevaX, transform.position.y, transform.position.z);
+
+            // ¡Acá está la magia! Cambiamos el dibujo al azar
+            int indiceAzar = Random.Range(0, misDibujosDeSuelo.Length);
+            sr.sprite = misDibujosDeSuelo[indiceAzar];
         }
     }
 }
-
-
